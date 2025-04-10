f346a3c75ebaebea5fff035a110f1aabcac10d2c
avinashpathy <avinashpathy2001@gmail.com>
2025-04-10 23:25:16 +0530

    polymorphism

---
+++ object-oriented-programming/oop-encapsulation.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// Access Modifier - Public, Protected, Private
+// Everything inside class in C++ is private by default
+
+// Encapsulation - Bundling data and methods together into a single unit and preventing other classes to directly access them
+// Using Getters and Setters methods which can be used to access the data
+
+class Employee
+{
+private:
+    string Name;
+    string Company;
+    int Age;
+
+public:
+    Employee(string name, string company, int age)
+    {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+
+    void setName(string name) // Setter
+    {
+        Name = name;
+    }
+    void getName() // Getter
+    {
+        cout << Name << endl;
+    }
+
+    void setAge(int age) // Setter
+    {
+        if (Age >= 18)
+        {
+            Age = age;
+        }
+    }
+    void getAge() // Getter
+    {
+        cout << Age << endl;
+    }
+    void IntroduceYourself()
+    {
+        cout << Name << " " << Company << " " << Age << endl;
+    }
+};
+
+int main()
+{
+    Employee emp1 = Employee("Avinash", "Koch", 24);
+    Employee emp2 = Employee("Shavinash", "Dock", 35);
+    // emp1.Name = "Avinash";
+    // emp1.Company = "Koch";
+    // emp1.Age = 24;
+    emp1.setName("Alok");
+    emp1.getName();
+    emp1.IntroduceYourself();
+    // Employee emp2 = Employee("John", "Google", 24); // Parameterized Constructor
+    // emp2.IntroduceYourself();
+    // emp2.Name = "John";
+    // emp2.Company = "Amazon";
+    // emp2.Age = 24;
+    // emp2.IntroduceYourself(); // o/p -   76732688  (default constructor) constructor with default values -  we didn't provide any values
+}
